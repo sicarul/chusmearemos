fb31b1cbc6f3423be067bb028269e2c01df6d144
Pablo Seibelt (Sicarul) <pabloseibelt@gmail.com>
2015-03-05 03:10:12 -0300

    Compress database

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
 grunt build
 cd dist
 s3cmd sync . s3://chusmearemos.sicarul.com
+cd data/
+gzip -9 escuchas.json
+mv escuchas.json.gz escuchas.json
+s3cmd sync --add-header="Content-Encoding:gzip" escuchas.json s3://chusmearemos.sicarul.com/data/escuchas.json
 cd ..
