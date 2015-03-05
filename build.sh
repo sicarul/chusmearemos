37ba6b4a982e0867771c83d04f245fa493e8fb43
Pablo Seibelt <pabloseibelt@gmail.com>
2015-03-05 12:01:41 -0300

    Fix build

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
 grunt build
-cd dist
-s3cmd sync . s3://chusmearemos.sicarul.com
-cd data/
+cd dist/data
 gzip -9 escuchas.json
 mv escuchas.json.gz escuchas.json
+cd ..
+s3cmd sync . s3://chusmearemos.sicarul.com --exclude="escuchas.json"
+cd data
 s3cmd sync --add-header="Content-Encoding:gzip" escuchas.json s3://chusmearemos.sicarul.com/data/escuchas.json
 cd ..
